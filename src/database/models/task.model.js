17a63761720b1a0ac60252a25f75fc11ef7694ba
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-05-02 17:22:57 -0300

    feat: add task model

---
+++ src/database/models/task.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const TaskSchema = Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const TaskModel = model("Task", TaskSchema);
+module.exports = TaskModel;
