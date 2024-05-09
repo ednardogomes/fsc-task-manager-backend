3708d95c09a52b25c0dc614b195aae565dfe3be5
Ednardo Gomes <ednardo_10.dev@hotmail.com>
2024-05-09 12:40:00 -0300

    feat: move get tasks endpoint to a controller

---
+++ src/routes/task.routes.js
@@ -0,0 +1,87 @@
+const express = require("express");
+
+const TaskModel = require("../models/task.model");
+const TaskController = require("../controller/task.controllers");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  return new TaskController(req, res).getTasks();
+});
+
+router.get("//:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    const task = await TaskModel.findById(taskId);
+
+    if (!task) {
+      return res.status(404).send("Essa tarefa não foi encontrada");
+    }
+
+    return res.status(200).send(task);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newTask = new TaskModel(req.body);
+
+    await newTask.save();
+
+    res.status(201).send(newTask);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const taskData = req.body;
+
+    const taskToUpdate = await TaskModel.findById(taskId);
+
+    const allowedUpdates = ["isCompleted"];
+    const requestedUpdates = Object.keys(taskData);
+
+    for (update of requestedUpdates) {
+      if (allowedUpdates.includes(update)) {
+        taskToUpdate[update] = taskData[update];
+      } else {
+        return res
+          .status(500)
+          .send("Um ou mais campos inseridos não são editáveis");
+      }
+    }
+
+    await taskToUpdate.save();
+    // return res.status(200).send(taskToUpdate);
+
+    return res.status(200).send(updatedTask);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+
+    const taskToDelete = await TaskModel.findById(taskId);
+
+    if (!taskToDelete) {
+      return res.status(404).send("Essa tarefa não foi encontrada");
+    }
+
+    const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+    res.status(200).send(deletedTask);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
